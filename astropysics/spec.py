e87fd3bafea51aeddd39c686a76b6ebc19a8f4a9
Erik Tollerud <erik.tollerud@gmail.com>
2008-12-17 18:21:43 -0800

    various Flux fixes, added spline interpolation.  zfind in progress

--- astropysics/spec.py
+++ astropysics/spec.py
@@ -8,24 +8,45 @@ except ImportError:
     print 'Spylot not found - some spec package functions may not work correctly'
     
 class Flux(object):
-    def __init__(self,x,flux,ivar=None,type='wl'):
+    """
+    Represents a flux/luminosity/intensity
+    
+    Note that operations are performed in-place, and properties retrieve the 
+    same versions that are changed (except ivar)
+    """
+    def __init__(self,x,flux,err=None,ivar=None,unit='wl',copy=True):
+        """
+        sets the x-axis values and the flux.  Optionally, an error can be
+        given by supplying an error or an inverse variance (bot not both)
         
-        x,flux = np.array(x),np.array(flux)
+        copy determines if the inputs will be copied if they are already arrays
+        """
+        
+        x = np.array(x,copy=copy)
+        flux = np.array(flux,copy=copy)
         if x.shape != flux.shape:
             raise ValueError("x and flux don't match shapes")
         
-        if ivar is None:
-            ivar = np.zeros_like(flux)
-        elif ivar.shape != flux.shape:
-            raise ValueError("ivar and flux don't match shapes")
+        if ivar is None and err is None:
+            err = np.zeros_like(flux)
+        elif ivar is not None:
+            if ivar.shape != flux.shape:
+                raise ValueError("ivar and flux don't match shapes")
+            err = np.array(ivar,copy=False)**-0.5
+        elif err is not None:
+            if err.shape != flux.shape:
+                raise ValueError("err and flux don't match shapes")
+            err=np.array(err,copy=copy)
+        else:
+            raise ValueError("can't set both err and ivar at the same time")
         
-        self._type,self._unit,self._scaling = self._strToType(type)
+        self._phystype,self._unit,self._scaling = self._strToUnit(unit)
         
         self._x = x
         self._flux = flux
-        self._err = ivar**-0.5
+        self._err = err
         
-    def _strToType(self,typestr):
+    def _strToUnit(self,typestr):
         u = typestr.lower()
         if u == 'wl' or u == 'lambda' or u == 'ang' or u == 'angstroms' or u == 'wavelength-angstrom':
             val =  'wavelength-angstrom'
@@ -48,7 +69,7 @@ class Flux(object):
         elif u == 'thz' or u == 'frequency-THz':
             val =  'frequency-THz'
             scaling=1e12
-        elif u == 'e' or u == 'energy-eV':
+        elif u == 'e' or u == 'en' or u == 'energy' or u == 'energy-eV':
             from .constants import ergperev
             val =  'energy-eV'
             scaling=ergperev
@@ -59,12 +80,13 @@ class Flux(object):
             val =  'energy-J'   
             scaling=1e-7
         else:
-            raise ValueError('unrecognized type')
+            raise ValueError('unrecognized unit')
         
-        tyep,unit = val.split('-')
-        return type,unit,scaling
+        phystype,unit = val.split('-')
+        return phystype,unit,scaling
         
     def _convertType(self,oldtype,newtype,oldscale,newscale):
+        #TODO:fix wl to freq
         from .constants import c,h
         
         x,flux,err = self._x/oldscale,self._flux*oldscale,self._err*oldscale # convert to cgs
@@ -107,32 +129,47 @@ class Flux(object):
             raise ValueError('unrecognized newtype')
         
         return x*newscale,flux*fluxscale/newscale,err*fluxscale/newscale
-        
+    #------------------------Properties--------------------------------->
+    @property
+    def npix(self):
+        return self._x.shape[-1]
+    
+    @property
+    def shape(self):
+        return self._x.shape
+    
     def _getFlux(self):
-        return self._flux.copy()
+        return self._flux
     def _setFlux(self,flux):
-        flux = np.array(flux)
-        if flux.shape != self._x.shape:
-            raise ValueError("new flux doesn't match x shape")
-        self._flux = np.array(flux)
+        flux = np.array(flux,copy=False)
+        if flux.shape != self._flux.shape:
+            raise ValueError("new flux doesn't match old flux shape")
+        self._flux[:] = np.array(flux)
     flux = property(_getFlux,_setFlux)
     
     def _getNFlux(self):
-        raise NotImplementedError
+        from .constants import h
+        
+        oldtype = self.unit
+        try:
+            self.unit = 'hz'
+            return self._flux/h/self._x
+        finally:
+            self.type = oldtype
     def _setNFlux(self,nflux):
         nflux = np.array(nflux)
-        if nflux.shape != self._x.shape:
-            raise ValueError("new flux doesn't match x shape")
+        if nflux.shape != self._flux.shape:
+            raise ValueError("new flux doesn't match flux shape")
         raise NotImplementedError
         self._flux = 42
     nflux = property(_getNFlux,_setNFlux)
     
     def _getErr(self):
-        return self._err.copy()
+        return self._err
     def _setErr(self,err):
-        if err.shape != self._flux.shape:
-            raise ValueError("new err doesn't match flux shape")
-        self._err=np.array(err)
+        if err.shape != self._err.shape:
+            raise ValueError("new err doesn't match old err shape")
+        self._err[:]=np.array(err)
     err = property(_getErr,_setErr)
     
     def _getIvar(self):
@@ -144,7 +181,7 @@ class Flux(object):
     ivar = property(_getIvar,_setIvar)
     
     def _getX(self):
-        return self._x.copy()
+        return self._x
     def _setX(self,x):
         x = np.array(x)
         if x.shape != self._flux.shape:
@@ -153,21 +190,24 @@ class Flux(object):
     x = property(_getX,_setX)
     
     def _getUnit(self):
-        return self._type+'-'+self._unit
+        return self._phystype+'-'+self._unit
     def _setUnit(self,typestr):
-        newtype,newunit,newscaling = self._strToType(typestr)
+        newtype,newunit,newscaling = self._strToUnit(typestr)
         
-        x,flux,err = self._convertType(self._type,newtype,self._scaling,newscaling)
+        x,flux,err = self._convertType(self._phystype,newtype,self._scaling,newscaling)
         
-        self._x = x
-        self._flux = flux
-        self._err = err
-        self._type = newtype
+        self._x[:] = x
+        self._flux[:] = flux
+        self._err[:] = err
+        self._phystype = newtype
         self._unit = newunit
         self._scaling = newscaling
     unit = property(_getUnit,_setUnit)
     
     #<----------------------Tests/Info--------------------------------->
+    def __len__(self):
+        return len(self._x)
+    
     def getDx(self,mean=True):
         """
         get the spacing of the x-axis, which is always 1 element shorter than x
@@ -210,7 +250,7 @@ class Flux(object):
     
     #<----------------------Operations---------------------------->
     
-    def smooth(self,width=1,filtertype='gaussian',replace=False):
+    def smooth(self,width=1,filtertype='gaussian',replace=True):
         """
         smooths the flux in this object by a filter of the given filtertype 
         (can be either 'gaussian' or 'boxcar')
@@ -237,57 +277,96 @@ class Flux(object):
         
         return smoothed
     
-    def linearize(self,lower=None,upper=None,n=None):
+    def resample(self,newx,interpolation='linear',replace=True,**kwargs):
+        """
+        this interpolates the flux to populate the supplies x-axis
+        
+        kwargs go into the interpolation routine as described below
+        
+        interpolations can be:
+        'linear': simple linear interpolation
+        'spline': a k-order spline with smoothing factor s is used, where s and 
+        k are set by kwargs.  if the 'save' kwarg is True, the spline is saved
+        and will be used for subsequent resamplings.  if 'clear' is True, the 
+        existing spline will be cleared and a new spline will be calculated.
+        
+        WARNING: this does not treat the errors properly yet - currently just interpolates
+        
+        returns newx,newflux,newerr
+        """
+        if interpolation == 'linear':
+            newflux = np.interp(newx,self._x,self._flux)
+            #TODO: fix errors
+            newerr = np.interp(newx,self._x,self._err)
+        elif 'spline' in interpolation:
+            from scipy.interpolate import UnivariateSpline
+            
+            s = kwargs.pop('s',None)
+            k = kwargs.pop('k',3)
+            save = kwargs.pop('save',False)
+            clear = kwargs.pop('clear',False)
+            
+            fspline = espline = None
+            if hasattr(self,'_spline') and not (clear or save):
+                fspline,espline = self._spline
+                
+                    
+            if fspline is None:
+                fspline = UnivariateSpline(self._x,self._flux,k=k,s=s)
+            if espline is None:
+                espline = UnivariateSpline(self._x,self._flux,k=k,s=s)
+                
+            if save:
+                self._spline = (fspline,espline)
+            
+            newflux = fspline(newx)
+            #TODO: fix errors
+            newerr = espline(newx)
+        else:
+            raise ValueError('unrecognized interpolation technique')
+        
+        if len(kwargs)>0:
+            raise TypeError('unexpected keyword %s'%kwargs.keys()[0])
+        
+        if replace:
+            if newx.shape != self._x.shape:
+                raise ValueError("can't replace if new x-axis shape doesn't match old")
+            
+            self._x[:] = newx 
+            self._flux[:] = newflux
+            self._err[:] = newerr   
+        return newx,newflux,newerr
+    
+    def linearize(self,lower=None,upper=None,**kwargs):
         """
         convinience function for resampling to an equally-spaced linear x-axis
         
         if lower or upper are None, the upper and lower x values are used
-        if n is None, the same number of samples are used
+        kwargs go into Flux.resample
         """
         if lower is None:
             lower = np.min(self._x)
         if upper is None:
             upper = np.max(self._x)
-        if n is None:
-            n = self._x.shape[0]
         
-        newx = np.linspace(lower,upper,n)
-        return self.resample(newx)
+        newx = np.linspace(lower,upper,self.npix)
+        return self.resample(newx,**kwargs)
     
-    def logify(self,lower=None,upper=None,n=None):
+    def logify(self,lower=None,upper=None,**kwargs):
         """
         convinience function for resampling to an x-axis that is equally spaced
         in logarithmic bins.  Note that lower and upper are the x-axis values 
         themselves, NOT log(xvalue)
         
         if lower or upper are None, the upper and lower x values are used
-        if n is None, the same number of samples are used
         """
         if lower is None:
             lower = np.min(self._x)
         if upper is None:
             upper = np.max(self._x)
-        if n is None:
-            n = self._x.shape[0]
         
-        newx = np.logspace(np.log10(lower),np.log10(upper),n)
-        return self.resample(newx)
-    
-    def resample(self,newx,interpolation='linear'):
-        """
-        this interpolates the flux to populate the supplies x-axis
-        
-        currently the only available interpolation is 'linear'
-        
-        WARNING: this does not treat the errors properly yet - currently just interpolates
-        """
-        newflux = np.interp(newx,self._x,self._flux)
-        #TODO: fix errors
-        newerr = np.interp(newx,self._x,self._err)
-        
-        self._x = newx 
-        self._flux = newflux
-        self._err = newerr       
+        newx = np.logspace(np.log10(lower),np.log10(upper),self.npix)
+        return self.resample(newx,**kwargs)
         
     
     def plot(self,fmt='-',clf=True,log=False,**kwargs):
@@ -301,6 +380,81 @@ class Flux(object):
         else:
             plot = plt.plot
         return plot(self.x,self.flux,fmt,**kwargs)
+    
+
+def zfind(object,templates,lags=True,dochecks = True):
+    """
+    Computes the template with the best-fitting chi-squared
+    
+    lags specify the possible pix-lags to try - if True, it will try all 
+    possibilities, if a sequence of numbers, will try all supplied lags, and 
+    if False/None, does no lag
+    
+    dochecks makes sure the spectra are all logarithmically spaced and interpolates
+    if necessary
+    
+    returns bestz,chi2s,zs
+    """
+    from operator import isSequenceType,isMappingType
+    
+    if dochecks:
+        from warnings import warn
+        
+        if isinstance(object,Flux):
+            pass
+        elif isSequenceType(object):
+            object = Flux(*object)
+        elif isMappingType(object):
+            object = Flux(**object)
+        else:
+            raise TypeError("couldn't interpret input object")
+        
+        if not object.isLogarithmic():
+            warn('object is not logarithmic - interpolating to logarithmic spacing')
+            object.logify()
+        
+        
+        if isinstance(templates,Flux):
+            templates=[templates]
+        elif isSequenceType(templates):
+            temptemplates=[]
+            for t in templates:
+                if isinstance(object,Flux):
+                    temptemplates.append(t)
+                elif isSequenceType(object):
+                    temptemplates.append(Flux(*t))
+                elif isMappingType(object):
+                    temptemplates.append(Flux(**t))
+                else:
+                    raise TypeError("couldn't interpret input object")
+            templates = temptemplates
+            del temptemplates
+        else:
+            raise TypeError("input templates not a sequence")
+        for i,t in enumerate(templates):
+            if not t.isLogarithmic():
+                warn('template %i is not logarithmic - interpolating to logarithmic spacing'%i)
+                t.logify()
+        #TODO:align templates to object - for now just assume its right
+    
+    x = object.x
+        
+    objflux = object.flux
+    ierr = 1/object.err
+    tempfluxes=np.array([t.flux for t in templates])
+    
+    #go over all possible lags, computer chi2s
+    b = objflux * ierr
+
+    mmatrix = tempfluxes * outer(ierr,ones(tempfluxes.shape[0]))
+    
+    raise NotImplementedError
+    return bestz,chi2s,zs
+    
+    
+
+    
+#<---------------------Data loading functions---TODO:move to plugin-like?------>
 
 def load_deimos_spectrum(fn,plot=True,extraction='horne',retdata=False,smoothing=None):
     """
@@ -406,7 +560,7 @@ def load_deimos_templates(fns):
                         warn('could not infer spectrum name - using filename %s as key name'%k)
                         
                     xd[k] = x
-                    tempd[k]=spec.copy()
+                    tempd[k]=spec.copy() #TODO: see if its better to not copy here
                     
         finally:
             f.close()
