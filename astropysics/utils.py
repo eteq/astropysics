ad3a60df27458f7289ccda73e02de8f1dfd51cc9
Erik Tollerud <erik.tollerud@gmail.com>
2010-03-22 18:38:46 -0700

    typo bugfix in utils

--- astropysics/utils.py
+++ astropysics/utils.py
@@ -116,7 +116,7 @@ def fpickle(object,fileorname,usecPickle=True,protocol=None,append=False):
         protocol = pickle.HIGHEST_PROTOCOL
     
     if isinstance(fileorname,basestring):
-        f = open(filename,'a' if append else 'w')
+        f = open(fileorname,'a' if append else 'w')
         close = True
     else:
         f = fileorname
