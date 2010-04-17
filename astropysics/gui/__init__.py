e5085e76de932a439099f90dc9cbebb9f3c21357
Erik Tollerud <erik.tollerud@gmail.com>
2010-04-17 15:48:53 -0700

    doc updates for guis and added the application classes to the base gui module

--- astropysics/gui/__init__.py
+++ astropysics/gui/__init__.py
@@ -7,31 +7,22 @@ Note that this module makes heavy use of Enthought Traits and TraitsGUI
 (http://code.enthought.com/projects/traits/) -- if it is not installed, 
 nearly everything in this package will raise an exception.
 
-GUI tools (each in their own modules:
+GUI tools (each in their own modules):
 
 * spectarget: a tool for making slitmasks from fiducial CMDs and photometric
- catalogs.  Currently only has direct support for Keck/DEIMOS
+  catalogs.  Currently only has direct support for Keck/DEIMOS
 * fitgui: tools for interactively fitting ``astropysics.models`` models 
   to data.
 * spylot: spectrum plotter
 
-tools can aslo be invoked from functions:
-
-* spec_target : creates and shows a spectarget UI
-* fit_data : creates and shows a fitgui UI
-* fit_data_multi : creates and shows a fitgui UI for dimensions > 2 (requires 
-  mayavi to be installed)
-* spylot_specs : creates and shows a spylot UI
+Convinence functions for launching the apps and the primary application classes
+are also aliased in this module for ease of use.
  
 """
 try:
-    from spectarget import spec_target
-    from fitgui import fit_data
-    from spylot import spylot_specs
-    try:
-        from fitgui import fit_data_multi
-    except ImportError:
-        pass #if mayavi is not present, a warning will have already been raised
+    from spectarget import spec_target,SpecTarget
+    from fitgui import fit_data,FitGui
+    from spylot import spylot_specs,Spylot
 except ImportError,e:
     if 'traits' in e.message:
         from warnings import warn
--- docs/gui/intro.rst
+++ docs/gui/intro.rst
@@ -5,9 +5,22 @@ Astropysics includes a variety of graphical applications for various data analys
 Their full power is acheived when used interactively or as part of scripts, but some 
 operate as stand-alone command-line tools when the situation warrants.
 
+Note that these applications make heavy use of `Enthought Traits <http://code.enthought.com/projects/traits/>`_ and the associated `Enthought Tools Suite <http://code.enthought.com/projects/ets/>`_ 
+ if they are not installed, most of these will not function.
+
 .. toctree::
    :maxdepth: 2
    
    spylot
    fitgui
    spectarget
+   
+   
+the :mod:`gui` module also imports the convinience functions and primary application classes for these GUIs.  These include:
+
+* :class:`FitGui <astropysics.gui.fitgui.FitGui>`
+* :func:`fit_data <astropysics.gui.fitgui.fit_data>`
+* :class:`Spylot <astropysics.gui.spylot.Spylot>`
+* :func:`spylot_specs <astropysics.gui.spylot.spylot_specs>`
+* :class:`SpecTarget <astropysics.gui.spectarget.SpecTarget>`
+* :func:`spec_target <astropysics.gui.spectarget.spec_target>`
