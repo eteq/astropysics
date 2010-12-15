568dab3da834e8b402f8ee1355e2bfd236461f69
Erik Tollerud <erik.tollerud@gmail.com>
2010-12-14 17:24:54 -0800

    added configobj

---
+++ astropysics/external/__init__.py
@@ -0,0 +1,21 @@
+#Copyright 2010 Erik Tollerud
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+This package contains small external packages that are included with astropysics 
+to simplify installation.  Currently, this includes:
+
+    * ConfigObj v4.7.2  (http://www.voidspace.org.uk/python/configobj.html)
+
+"""
