98c4d3c882c7913772da6dd1f2cce72b80b93442
Erik Tollerud <erik.tollerud@gmail.com>
2011-04-21 12:35:41 -0700

    added count_lines command to setup

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from distribute_setup import use_setuptools
 use_setuptools()
 from setuptools import setup,find_packages
 from distutils.command.build_py import build_py as du_build_py
+from distutils.core import Command
 
 from astropysics.version import version as versionstr
 from astropysics.config import _recpkgs,_guipkgs
@@ -68,8 +69,80 @@ try:
 except ImportError: #sphinx not present
     apy_build_sphinx = None
     
+    
+#command to count the number of lines of code (mostly for curiosity's sake) in the main dirs
+class CountLines(Command):
+    # Brief (40-50 characters) description of the command
+    description = "Print the number of lines in the major directories to the terminal."
 
-cmdclassd = {'build_py' : apy_build_py}
+    # List of option tuples: long name, short name (None if no short
+    # name), and help string.
+    user_options = [('includeempty', 'e',
+                     "Include empty lines in the count"),
+                   ]
+    
+    def initialize_options (self):
+        self.includeempty = False
+
+    def finalize_options (self):
+        pass
+    
+    def visit_files(self,lists,dirname,fnames):
+        lcountlist,fcountlist = lists
+        from os import path
+        
+        #prefilter for valid extentions
+        if dirname != 'scripts':
+            fnames = [fn for fn in fnames if (fn.endswith('.py') or fn.endswith('.pyx')) ]
+        
+        cnt = 0
+        for fn in fnames:
+            fn = path.join(dirname,fn)
+            with open(fn) as f:
+                if self.includeempty:
+                    for l in f:
+                        cnt += 1
+                else:
+                    for l in f:
+                        if l.strip()!='':
+                            cnt += 1
+        
+        lcountlist.append(cnt)
+        fcountlist.append(len(fnames))
+
+    def run(self):
+        from os import path
+        
+        dir,name = path.split(__file__)
+        
+        apydir = path.join(dir,'astropysics')
+        apyllst,apyflst = [],[]
+        path.walk(apydir,self.visit_files,(apyllst,apyflst))
+        self.apylinecount = sum(apyllst)
+        self.apyfilecount = sum(apyflst)
+        
+        scrdir = path.join(dir,'scripts')
+        scrllst,scrflst = [],[]
+        path.walk(scrdir,self.visit_files,(scrllst,scrflst))
+        self.scrlinecount = sum(scrllst)
+        self.scrfilecount = sum(scrflst)
+        
+        tstdir = path.join(dir,'tests')
+        tstllst,tstflst = [],[]
+        path.walk(tstdir,self.visit_files,(tstllst,tstflst))
+        self.tstlinecount = sum(tstllst)
+        self.tstfilecount = sum(tstflst)
+        
+        self.linecount = self.apylinecount + self.scrlinecount + self.tstlinecount
+        self.filecount =  self.apyfilecount + self.scrfilecount + self.tstfilecount
+        
+        print 'Astropysics source directory has %i lines in %i files'%(self.apylinecount,self.apyfilecount)
+        print 'Scripts directory has %i lines in %i files'%(self.scrlinecount,self.scrfilecount)
+        print 'Tests directory has %i lines in %i files'%(self.tstlinecount,self.tstfilecount)
+        print 'Total %i lines in %i files'%(self.linecount,self.filecount)
+         
+
+cmdclassd = {'build_py' : apy_build_py,'count_lines':CountLines}
 if apy_build_sphinx is not None:
     cmdclassd['build_sphinx'] = apy_build_sphinx
     
